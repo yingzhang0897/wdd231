43e924aec64b4e8c6cec71d9676de006c273f3a2
“yingzhang0897” <yinganticpc@gmail.com>
2024-10-22 09:58:37 -0700

    update pick-of-week.js

--- kindergarten/scripts/pick-of-week.js
+++ kindergarten/scripts/pick-of-week.js
@@ -1,14 +1,14 @@
 export const pickOfTheWeek = [
     {
-        name: "Emma Johnson",
-        age: 5,
-        img: "images/hero.jpeg",
-        description: "Emma loves painting and always helps her friends during art time. She is known for her kindness and creativity."
+        name: "Isaac Johnson",
+        age: 3,
+        img: "images/Isaac.webp",
+        description: "Isaac loves painting and always helps his friends during art time. He is known for his kindness and creativity."
     },
     {
-        name: "Liam Smith",
+        name: "Luke Bushing",
         age: 6,
-        img: "images/hero.jpeg",
-        description: "Liam enjoys building with blocks and has a knack for solving puzzles. He is always ready to explore and share his ideas with others."
+        img: "images/Luke.webp",
+        description: "Luke enjoys building with blocks and has a knack for solving puzzles. He is always ready to explore and share his ideas with others."
     }
 ];
