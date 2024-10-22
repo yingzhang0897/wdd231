703e250fe5b916a578697581197d7a2b4994d5f1
“yingzhang0897” <yinganticpc@gmail.com>
2024-10-22 12:56:18 -0700

    add program-term.js

---
+++ kindergarten/scripts/program-term.js
@@ -0,0 +1,45 @@
+export const programLevels = [
+    {
+        name: 'Preschool',
+        ageGroup: '3-5years',
+        description: 'Our Preschool program focuses on early childhood development through fun and engaging activities that promote social, emotional, and cognitive growth.'
+    },
+    {
+        name: 'Kindergarten',
+        ageGroup: '5-6 years',
+        description: 'The Kindergarten program provides a foundation for reading, writing, math, and social skills, preparing children for elementary school in a nurturing environment.'
+    },
+    {
+        name: 'After-school Activities',
+        ageGroup: '5 years above',
+        description: 'Our After-school activities include basketball, soccer, piano, hip-hop dance basics and Chinese calligraphy.'
+    }
+];
+
+export const terms = [
+    { 
+        name: 'winter term',
+        duration: '14 weeks',
+        tuition: '$ 1,400',
+        date: 'January 6 - April 11'
+    },
+    {
+        name: 'spring term',
+        duration: '14 weeks',
+        tuition: '$ 1,400',
+         date: 'April 14 - July 18'
+    },
+    {
+        name: 'summer term',
+        duration:'5 weeks',
+        tuition: '$ 500',
+        date: 'July 21 - August 22'
+    },
+    {
+        name: 'fall term',
+        duration: '14 weeks',
+        tuition: '$ 1,400',
+        date: 'September 1 - December 13'
+    }
+];
+
