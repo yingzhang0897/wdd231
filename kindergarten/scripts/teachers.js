8f07f91d41ed219e22b42698ac32ac6d9ced9501
“yingzhang0897” <yinganticpc@gmail.com>
2024-10-22 21:19:35 -0700

    add teachers.js

---
+++ kindergarten/scripts/teachers.js
@@ -0,0 +1,72 @@
+export const teachers = [
+    { 
+        img: 'images/teachers/Lina.webp',
+        name: 'Lina',
+        age: 23,
+        expertise: 'Math',
+        personalityTraits: 'patient',
+    },
+    {
+        img: 'images/teachers/Jack.webp',
+        name: 'Jack',
+        age: 30,
+        expertise: 'Science',
+        personalityTraits: 'inquisitive',
+    },
+    {
+        img: 'images/teachers/Sophia.webp',
+        name: 'Sophia',
+        age: 28,
+        expertise: 'English',
+        personalityTraits: 'enlightening',
+    },
+    {
+        img: 'images/teachers/Emily.webp',
+        name: 'Emily',
+        age: 35,
+        expertise: 'Art',
+        personalityTraits: 'creative'
+    },
+    {
+        img: 'images/teachers/Michael.webp',
+        name: 'Michael',
+        age: 29,
+        expertise: 'Physical Education',
+        personalityTraits: 'energetic',
+    },
+    {
+        img: 'images/teachers/Lily.webp',
+        name: 'Lily',
+        age: 32,
+        expertise: 'Chinese',
+        personalityTraits: 'humorous',
+    },
+    {
+        img: 'images/teachers/Amanda.webp',
+        name: 'Amanda',
+        age: 27,
+        expertise: 'Music',
+        personalityTraits: 'enthusiastic'
+    },
+    {
+        img: 'images/teachers/Chloe.webp',
+        name: 'Chloe',
+        age: 26,
+        expertise: 'Dance',
+        personalityTraits: 'cheerful'
+    },
+    {
+        img: 'images/teachers/Oliver.webp',
+        name: 'Oliver',
+        age: 31,
+        expertise: 'Nutrition',
+        personalityTraits: 'caring'
+    },
+    {
+        img: 'images/teachers/Ethan.webp',
+        name: 'Ethan',
+        age: 40,
+        expertise: 'Janitor',
+        personalityTraits: 'responsible'
+    }
+];
