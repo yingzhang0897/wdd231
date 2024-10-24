ae63a651c856d110c3d1ac03c274d30f95423138
“yingzhang0897” <yinganticpc@gmail.com>
2024-10-23 23:22:14 -0700

    add kids.js

---
+++ kindergarten/scripts/kids.js
@@ -0,0 +1,67 @@
+export const kids = [
+    {
+        name: "Isaac",
+        age: 3,
+        img: "images/kids/Isaac-3.webp"
+    },
+    {
+        name: "Isaac",
+        age: 4,
+        img: "images/kids/Isaac-4.webp"
+    },
+    {
+        name: "Isaac",
+        age: 5,
+        img: "images/kids/Isaac-5.webp"
+    },
+    {
+        name: "Isaac",
+        age: 6,
+        img: "images/kids/Isaac-6.webp"
+    },
+    {
+        name: "Rhelina",
+        age: 3,
+        img: "images/kids/Rhelina-3.webp"
+    },
+    {
+        name: "Rhelina",
+        age: 4,
+        img: "images/kids/Rhelina-4.webp"
+    },
+    {
+        name: "Rhelina",
+        age: 5,
+        img: "images/kids/Rhelina-5.webp"
+    },
+    {
+        name: "Rhelina",
+        age: 6,
+        img: "images/kids/Rhelina-6.webp"
+    },
+    {
+        name: "Luke",
+        age: 3,
+        img: "images/kids/Luke-3.webp"
+    },
+    {
+        name: "Luke",
+        age: 4,
+        img: "images/kids/Luke-4.webp"
+    },
+    {
+        name: "Luke",
+        age: 5,
+        img: "images/kids/Luke-5.webp"
+    },
+    {
+        name: "Luke",
+        age: 6,
+        img: "images/kids/Luke-6.webp"
+    },
+    {
+        name: "Luke",
+        age: 7,
+        img: "images/kids/Luke-7.webp"
+    }
+];
