4096a6b53418395859839c749de14f1e088cd8ce
“yingzhang0897” <yinganticpc@gmail.com>
2024-10-26 15:17:26 -0700

    add h4 to pick-of-week container

--- kindergarten/scripts/about-us.js
+++ kindergarten/scripts/about-us.js
@@ -62,6 +62,7 @@ document.addEventListener('DOMContentLoaded', () => {
                 });
     
                 // Append the image element to the pickSection
+                pickSection.appendChild(h4);
                 pickSection.appendChild(imgElement);
             });
     
